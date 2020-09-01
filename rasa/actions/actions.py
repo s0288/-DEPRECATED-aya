fcddd24328777802716fd8a5412fa8dfcc3ec20e
s0288 <alex.gansmann@gmail.com>
2020-09-01 20:52:14 +0200

    set action server to remind after 16 hours

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -133,7 +133,7 @@ class SetReminder16h(Action):
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
 
-        date = datetime.datetime.now() + datetime.timedelta(seconds=10)
+        date = datetime.datetime.now() + datetime.timedelta(hours=16)
         entities = tracker.latest_message.get("entities")
 
         reminder = ReminderScheduled(
