94971e9a659a7dbd6a6002fb97bacb937f586173
s0288 <alex.gansmann@gmail.com>
2020-09-19 21:05:25 +0200

    shortened reminder dispatch to 2 hours

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -158,7 +158,7 @@ class SetReminder16h(Action):
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
 
-        date = datetime.datetime.now() + datetime.timedelta(hours=16) # 16 hours
+        date = datetime.datetime.now() + datetime.timedelta(minutes=120) # 16 hours
         entities = tracker.latest_message.get("entities")
 
         reminder = ReminderScheduled(
