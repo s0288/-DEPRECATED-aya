75ba44a5800007e4bfc144583fd34cd289c91fad
s0288 <alex.gansmann@gmail.com>
2020-12-21 18:24:04 +0100

    aya notifies on fasting progress in fixed intervals (16, 20, 24, 48, 96, ... hours)

--- rasa/actions.py
+++ rasa/actions.py
@@ -26,15 +26,28 @@ class ActionStartFast(Action):
                 domain: Dict[Text, Any],
             ) -> List[Dict[Text, Any]]:
 
-        name = 'start_fast'
         created_at = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-        platform_user_id = tracker.sender_id
-        platform_name = 'Telegram'
-        value = created_at
-        received_at = created_at
 
         # set slots and schedule a reminder
         return [SlotSet("is_fasting", 1), SlotSet("start_fast", created_at), FollowupAction("action_set_reminder_fast")]
+
+
+def _calc_fasting_since(start_fast):
+    '''
+        Subtract the start_fast (a timestamp) from the current time.
+        Parameters:
+            start_fast: timestamp
+        Returns:
+            hours_fasted: integer
+            mins_fasted: integer
+    '''
+    created_at = datetime.datetime.now()
+    time_fasted = created_at - start_fast
+    seconds_fasted = time_fasted.seconds + time_fasted.days*24*60*60
+    hours_fasted = seconds_fasted//3600 # round to nearest integer
+    mins_fasted = (seconds_fasted%3600)//60 # get remainder in mins
+    return hours_fasted, mins_fasted
+
 
 class ActionFastingSince(Action):
     """ calculate the time since the start of the fast """
@@ -51,33 +64,18 @@ class ActionFastingSince(Action):
         
         ## check if user is currently fasting
         start_fast = tracker.get_slot('start_fast')
-
-        ## if user is currently fasting, set text slots
-        ## calculate time_fasted        
-        try:
-            created_at = datetime.datetime.now()
-            # make sure start_fast has the right format
-            start_fast = datetime.datetime.strptime(str(start_fast), '%Y-%m-%d %H:%M:%S')
-
-            time_fasted = created_at - start_fast
-            days_fasted = time_fasted.days
-            seconds_fasted = time_fasted.seconds + time_fasted.days*24*60*60
-
-            hours_fasted = seconds_fasted//3600 # round to nearest integer
-            mins_fasted = (seconds_fasted%3600)//60 # get remainder in mins
-
-            # for singular, set values
-            mins_str = f'{mins_fasted} Minute' if mins_fasted == 1 else f'{mins_fasted} Minuten'
-            hours_str = f'{hours_fasted} Stunde' if hours_fasted == 1 else f'{hours_fasted} Stunden'
-
-            ## set is_fasting to 1 since the user is fasting
-            is_fasting = 1
-        ## if user is currently not fasting, set slots to None
-        except:
+        
+        if start_fast is None: # if user is currently not fasting, set slots to None
+            is_fasting = 0 # set is_fasting to 0 since the user is not fasting
             mins_str = '0 Minuten'
             hours_str = '0 Stunden'
-            ## set is_fasting to 0 since the user is not fasting
-            is_fasting = 0
+        else: # if user is currently fasting, set text slots
+            is_fasting = 1 # set is_fasting to 1 since the user is fasting
+            start_fast_dt = datetime.datetime.strptime(str(start_fast), '%Y-%m-%d %H:%M:%S')
+            hours_fasted, mins_fasted = _calc_fasting_since(start_fast_dt)
+            # if singular, set values
+            mins_str = f'{mins_fasted} Minute' if mins_fasted == 1 else f'{mins_fasted} Minuten'
+            hours_str = f'{hours_fasted} Stunde' if hours_fasted == 1 else f'{hours_fasted} Stunden'
 
         return [SlotSet("is_fasting", is_fasting), SlotSet("start_fast", str(start_fast)), SlotSet("hours_fasted", hours_str), SlotSet("mins_fasted", mins_str)]
 
@@ -96,12 +94,7 @@ class ActionEndFast(Action):
 
         ## add end_fast to event_db
         created_at = datetime.datetime.now()
-        name = 'end_fast'
         created_at = created_at.strftime('%Y-%m-%d %H:%M:%S')
-        platform_user_id = tracker.sender_id
-        platform_name = 'Telegram'
-        value = created_at
-        received_at = created_at
 
         return [SlotSet("is_fasting", 0), SlotSet("start_fast", None), SlotSet("hours_fasted", "0 Stunden"), SlotSet("mins_fasted", "0 Minuten"), FollowupAction("action_forget_reminder_fast")]
 
@@ -146,9 +139,26 @@ class SetReminderFast(Action):
         domain: Dict[Text, Any],
     ) -> List[Dict[Text, Any]]:
 
-        date = datetime.datetime.now() + datetime.timedelta(hours=16) # 16 hours
-        entities = tracker.latest_message.get("entities")
+        # based on fasting time, set reminder (e.g. hours=16, if at start of fast)
+        start_fast = tracker.get_slot('start_fast')
 
+        if start_fast is None: # if user is currently not fasting, assume he is at start of fast
+            reminder_in_x_hours = 16
+        else:
+            start_fast_dt = datetime.datetime.strptime(str(start_fast), '%Y-%m-%d %H:%M:%S')
+            hours_fasted, _ = _calc_fasting_since(start_fast_dt)
+            if hours_fasted < 16:
+                reminder_in_x_hours = 16
+            elif hours_fasted < 20: 
+                reminder_in_x_hours = 4
+            elif hours_fasted < 24: 
+                reminder_in_x_hours = 4
+            elif hours_fasted < 48:
+                reminder_in_x_hours = 24
+
+        date = datetime.datetime.now() + datetime.timedelta(hours=reminder_in_x_hours)
+        
+        entities = tracker.latest_message.get("entities")
         reminder = ReminderScheduled(
             "fast_reminder",
             trigger_date_time=date,
@@ -156,9 +166,8 @@ class SetReminderFast(Action):
             name="fast_reminder",
             kill_on_user_message=False,
         )
-
         print("setting reminder")
-
+        
         return [reminder]            
 
 
